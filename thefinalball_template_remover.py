07d386dbeb92171d45a5593185c99d0706258120
TheSandDoctor <twitter@markyrosongaming.com>
2019-03-13 14:44:04 -0700

    Rewritten
    Rewrote for current task

--- thefinalball_template_remover.py
+++ thefinalball_template_remover.py
@@ -2,106 +2,66 @@
 import mwclient, configparser, mwparserfromhell, re, argparse, sys
 from mwclient import *
 import json
+def getTransclusions(site,page,sleep_duration = None,extra=""):
+    cont = None;
+    pages = []
+    i = 1
+    while(1):
+        result = site.api('query',list='embeddedin',eititle=str(page),eicontinue=cont,eilimit=500,format='json')
+        print("got here")
+        if sleep_duration is (not None):
+            time.sleep(sleep_duration)
+        #res2 = result['query']['embeddedin']
+        for res in result['query']['embeddedin']:
+            print('append ' + res['title'])
+            pages.append(res['title'])
+            i +=1
+        try:
+            cont = result['continue']['eicontinue']
+            print("cont")
+        except NameError:
+            print("Namerror")
+            return pages
+        except Exception as e:
+            print("Other exception" + str(e))
+        #    print(pages)
+            return pages
 
 def call_home(site):
     page = site.Pages['User:DeprecatedFixerBot/status']
     text = page.text()
-    data = json.loads(text)["run"]["click_convert"]
+    data = json.loads(text)["run"]["thefinalball"]
     if str(data) == str(True):
         return True
     return False
-def allow_bots(text, user):
-    user = user.lower().strip()
-    text = mwparserfromhell.parse(text)
-    for tl in text.filter_templates():
-        if tl.name in ('bots', 'nobots'):
-            break
-    else:
-        return True
-    for param in tl.params:
-        bots = [x.lower().strip() for x in param.value.split(",")]
-        if param.name == 'allow':
-            if ''.join(bots) == 'none': return False
-            for bot in bots:
-                if bot in (user, 'all'):
-                    return True
-        elif param.name == 'deny':
-            if ''.join(bots) == 'none': return True
-            for bot in bots:
-                if bot in (user, 'all'):
-                    return False
-    return True
-def get_next_iter_item(some_iterable, window=1):
-    """
-    Get the item that will be in next iteration of the loop.
-    This will be useful for finding {{dead link}} templates.
-    This code is adapted from an answer to a StackOverflow question by user nosklo
-    https://stackoverflow.com/questions/4197805/python-for-loop-look-ahead/4197869#4197869
-    @param some_iterable Thing to iterate over
-    @param window How far to look ahead
-    """
-    items, nexts = tee(some_iterable, 2)
-    nexts = islice(nexts, window, None)
-    return zip_longest(items, nexts)
 def save_edit(page, utils, text):
      config = utils[0]
      site = utils[2]
-     dry_run = utils[4]
      original_text = text
 
      code = mwparserfromhell.parse(text)
-     for template in code.filter_templates():
-         if template.name.matches("nobots") or template.name.matches("Wikipedia:Exclusion compliant"):
-             if template.has("allow"):
-                 if "DeprecatedFixerBot" in template.get("allow").value:
-                     break # can edit
-             print("\n\nPage editing blocked as template preventing edit is present.\n\n")
-             return
+
 
      if not call_home(site):#config):
         raise ValueError("Kill switch on-wiki is false. Terminating program.")
-     edit_summary = 'Removed deprecated parameters from [[Template:Click]] using ' +  "[[User:" + config.get('enwikidep','username') + "| " + config.get('enwikidep','username') + "]]. Mistake? [[User talk:TheSandDoctor|msg TSD!]] (please mention that this is task #1!)"
+     edit_summary = 'Removed deprecated template [[Template:TheFinalBall]] using ' +  "[[User:" + config.get('enwikidep','username') + "| " + config.get('enwikidep','username') + "]]. Mistake? [[User talk:TheSandDoctor|msg TSD!]] (please mention that this is task #7!)"
      time = 0
      while True:
-         #content_changed = False
-         #text = page.edit()
-         #text = text.replace('[[Category:Apples]]', '[[Category:Pears]]')
          if time == 0:
              text = page.text()
          if time == 1:
         #     page = site.Pages[page.page_title]
              page.purge()
              original_text = site.Pages[page.page_title].text()
-         content_changed, text = remove_deprecated_params(original_text,dry_run)
+         content_changed, text = remove_deprecated_params(original_text)
          try:
-             if dry_run:
-                 print("Dry run")
-                 #Write out the initial input
-                 text_file = open("SandInput01.txt", "w")
-                 text_file.write(original_text)
-                 text_file.close()
-                 #Write out the output
-                 if content_changed:
-                     text_file = open("SandOutput01.txt", "w")
-                     text_file.write(text)
-                     text_file.close()
-                 else:
-                     print("Content not changed, don't print output")
-                 break
-             else:
-                if verbose:
-                    print("LIVE run")
-                #print("Would have saved here")
-                #break
-                #TODO: Enable
                 if not content_changed:
-                    if verbose:
-                        print("Content not changed, don't bother pushing edit to server")
                     break
-                #break
                 page.save(text, summary=edit_summary, bot=True, minor=True)
                 #print(page.page_title)
                 print("Saved page")
+                with open("thefinalball_saved.txt",'a+') as file:
+                    file.write(str(page.page_title))
                 if time == 1:
                     time = 0
                 break
@@ -115,62 +75,31 @@ def save_edit(page, utils, text):
              print(e)
          break
 
-def remove_deprecated_params(text,dry_run):
+def remove_deprecated_params(text):
     """
     Removes deprecated parameters from the {{Track listing}} template (and its redirects)
     @param text Page text to go over
-    @param dry_run boolean Whether or not this is a dry run (dry run = no live edit)
     @returns [content_changed, content] Whether content was changed,
     (if former true, modified) content.
     """
-#    print("In remove {}".format(dry_run))
     wikicode = mwparserfromhell.parse(text)
     templates = wikicode.filter_templates()
     content_changed = False
 
-    #TODO: Testing (dry run) only
-    if dry_run:
-        text_file = open("SandInput.txt","w")
-        text_file.write(text)
-        text_file.close()
-    #TODO: End dry run only
-
     code = mwparserfromhell.parse(text)
     for template in code.filter_templates():
-        if (template.name.matches("click") or template.name.matches("click-fixed")):
-            img = link = None
-            if template.has("image") and template.has("link"):
-                img = str(template.get("image").value)
-                link = str(template.get("link").value)
-                code.replace(template,"[[File:" + img + "|link=" + link + "]]")
-                content_changed = True
-            elif template.has("image"):
-                img = str(template.get("image").value)
-                code.replace(template,"[[File:" + img + "]]")
-                content_changed = True
+        if (template.name.matches("TheFinalBall") or template.name.matches("OGol")
+        or template.name.matches("Ogol")
+        or template.name.matches("TheFinalBall player") or template.name.matches("Zerozero")
+        or template.name.matches("Zerozero profile")):
+            code.remove(template)
+            content_changed = True
     return [content_changed, str(code)] # get back text to save
 
 def main():
     limited_run = True
     pages_to_run = 1
     offset = 0
-    category = True
-    dry_run = False
-    verbose = False
-
-    parser = argparse.ArgumentParser(prog='DeprecatedFixerBot Tracklisting fixer', description='''Goes through pages in the category [[:Category:Pages containing click using deprecated parameters]]
-    and removes deprecated parameters.''')
-    parser.add_argument("-dr", "--dryrun", help="perform a dry run (don't actually edit)",
-                    action="store_true")
-    parser.add_argument("-v","--verbose", help="Display more information when running",
-                    action="store_true")
-    args = parser.parse_args()
-    if args.dryrun:
-        dry_run = True
-        print("Dry run")
-    if args.verbose:
-        print("Verbose mode")
-        verbose = True
     #raise ValueError("for testing, dont want whole script running")
 
     site = mwclient.Site(('https','en.wikipedia.org'), '/w/')
@@ -183,8 +112,8 @@ def main():
         print(e)
         raise ValueError("Login failed.")
     counter = 0
-    utils = [config,None,site,False,dry_run]
-    for page in site.Categories['Track listings with deprecated parameters']:
+    utils = [config,None,site,False]
+    for page in getTransclusions(site,"Template:TheFinalBall"):
         if offset > 0:
             offset -= 1
             counter += 1
